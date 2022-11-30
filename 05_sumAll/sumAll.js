40d4468671b770025e53bf66e1280d7a73f752b7
zachlamont <zachlamont1@gmail.com>
2022-11-30 23:12:00 +1000

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function () {
+  let sumTotal = 0;
 
+  if (
+    typeof arguments[0] != "number" ||
+    typeof arguments[1] != "number" ||
+    arguments[0] < 0 ||
+    arguments[1] < 0
+  ) {
+    return "ERROR";
+  } else {
+    if (arguments[0] < arguments[1]) {
+      for (i = arguments[0]; i <= arguments[1]; i++) {
+        sumTotal += i;
+      }
+      return sumTotal;
+    } else {
+      for (i = arguments[1]; i <= arguments[0]; i++) {
+        sumTotal += i;
+      }
+      return sumTotal;
+    }
+  }
 };
 
 // Do not edit below this line
