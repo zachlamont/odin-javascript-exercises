d805eb1b244a4ca2b9644fdbaf220449ae19a734
zachlamont <zachlamont1@gmail.com>
2022-11-30 20:32:14 +1000

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let final = "";
 
+  if (num < 0) {
+    return "ERROR";  //returns ERROR if passed a negative number - ZL
+  } else {
+    for (i = num; i > 0; i--) {
+      final += string;
+    }
+    return final;
+  }
 };
 
 // Do not edit below this line
