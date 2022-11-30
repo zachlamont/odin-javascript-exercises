e8718d769f378a1de18f1bca966352b58998f106
zachlamont <zachlamont1@gmail.com>
2022-11-30 21:39:15 +1000

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let newString = [...string];
 
+  let reversed = "";
+
+  for (i = 0; i < string.length; i++) {
+    reversed += newString.pop();
+  }
+  return reversed;
 };
 
 // Do not edit below this line
