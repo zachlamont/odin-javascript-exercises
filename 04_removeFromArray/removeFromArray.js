94f64e2b1c94f18c7c16cdd3913cc86da7a0f51b
zachlamont <zachlamont1@gmail.com>
2022-11-30 22:10:42 +1000

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  array = arguments[0];
 
+  for (i = 1; i < arguments.length; i++) {
+    if (array.includes(arguments[i])) {
+      //if argument is included in the array
+      array.splice(array.indexOf(arguments[i]), 1); // remove that element
+    }
+  }
+  return array; //Success on the first try :) - ZL
 };
 
 // Do not edit below this line
